9393f122d08609623c9ea0cbd40f361a4a09711b
unkpell0 <marvelinohusca123@gmail.com>
2024-05-01 21:08:58 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente quaerat, impedit doloribus id dolorum illum dolores cupiditate non in ullam quas debitis eveniet nostrum quia, iste, ducimus ipsum expedita. Aperiam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
